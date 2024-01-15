7a7b7338aaa238ec42dbeda14db96453967ec3a4
wilsondagah <wdagah01@gmail.com>
2024-01-15 02:12:22 +0100

    test for withdraw

--- test/uint/FundMe.test.js
+++ test/uint/FundMe.test.js
@@ -12,7 +12,7 @@ describe("FundMe", async function () {
     const deploymentResult = await deployments.fixture(["all"]);
     //
     const fundMeAddress = deploymentResult["FundMe"]?.address;
-    deployer = fundMeAddress;
+    // deployer = fundMeAddress;
     fundMe = await ethers.getContractAt("FundMe", fundMeAddress);
 
     //
@@ -32,7 +32,7 @@ describe("FundMe", async function () {
       assert.equal(response, mockV3Aggregator);
     });
   });
-
+  //
   describe("fund", async function () {
     it("fails if you dont send enough ETH", async function () {
       expect(fundMe.fund({ value: 0 })).to.be.revertedWith("not enough funds");
@@ -43,6 +43,45 @@ describe("FundMe", async function () {
       const response = await fundMe.getAddressToAmountFunded(deployer.address);
       assert.equal(response.toString(), sendValue.toString());
     });
+    it("add funders to array", async function () {
+      await fundMe.fund({ value: sendValue });
+      const funder = await fundMe.getFunder(0);
+      const [deployer] = await ethers.getSigners();
+      assert.equal(funder, deployer.address);
+    });
   });
+  //
+  describe("withdraw", async function () {
+    beforeEach(async function () {
+      await fundMe.fund({ value: sendValue });
+    });
+
+    it("withdraw ETH from a single Funder", async function () {
+      // Arrange
+      const [deployer] = await ethers.getSigners();
+      const startFundMeBal = await ethers.provider.getBalance(fundMe.target);
+      console.log(startFundMeBal);
+      const startingDeployerBal = await ethers.provider.getBalance(deployer);
+      console.log(startingDeployerBal);
+      // Act
+      const transacRes = await fundMe.withdraw();
+      const transacReceipt = await transacRes.wait(1);
+
+      const endingFundingBal = await ethers.provider.getBalance(fundMe.target);
+      const endingDeployerBal = await ethers.provider.getBalance(
+        deployer.address
+      );
+      // gasCost
+      const { gasUsed, gasPrice } = transacReceipt;
+      const gasCost = gasUsed * gasPrice;
+      // Assert
+      assert.equal(endingFundingBal, 0);
+      assert.equal(
+        (startFundMeBal + startingDeployerBal).toString(),
+        (endingDeployerBal + gasCost).toString()
+      );
+    });
+  });
+
   //   beforeEach();
 });
